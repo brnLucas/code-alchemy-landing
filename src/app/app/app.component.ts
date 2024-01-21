72e479a2696ae924af67c79bc40cbc35569efa65
Breno Nascimento <breno.nascimento@fpf.br>
2024-01-20 21:15:46 -0400

    initial commit

---
+++ src/app/app/app.component.ts
@@ -0,0 +1,34 @@
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'code-alchemy-landing-page';
+  public darkModeButton: any;
+  public isDarkMode: boolean = false;
+
+  public setLigthMode(): void {
+    this.isDarkMode = false
+    this.darkModeButton = document.querySelector(".darkmode");
+    this.darkModeButton.style.color = "black";
+    this.darkModeButton.style.backgroundColor = "white";
+    document.body.classList.remove("darkmode");
+  }
+
+  public setDarkMode(): void {
+    this.isDarkMode = true
+    this.darkModeButton = document.querySelector(".darkmode");
+    this.darkModeButton.style.color = "orange";
+    document.body.classList.add("darkmode");
+
+    }
+    
+  }
+
